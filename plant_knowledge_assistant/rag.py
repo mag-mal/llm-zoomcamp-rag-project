1de5ddc8743a3392faaf4bc94accabd3f213d185
mag-mal <idmagda03@onet.eu>
2025-08-26 19:46:51 +0000

    Add rag flow

---
+++ plant_knowledge_assistant/rag.py
@@ -0,0 +1,137 @@
+import ingest
+from qdrant_client import QdrantClient, models
+from groq import Groq
+from dotenv import load_dotenv
+import os
+
+client = ingest.main()
+groq_api_key = os.getenv('GROQ_API_KEY')
+
+def multi_stage_search(query, limit = 5):
+    """
+    Perform a hybrid multi-stage search combining BM25 keyword search 
+    with semantic prefetch using Jina embeddings. 
+    Prefetch retrieves 10× the requested results for improved reranking, 
+    then returns the top matches with payloads.
+
+    Args:
+        query (str): Search query text.
+        limit (int, optional): Number of final results to return. Defaults to 5.
+
+    Returns:
+        list[models.ScoredPoint]: Ranked search results with payload data.
+    """
+    results = client.query_points(
+        collection_name=ingest.COLLECTION_NAME,
+        prefetch=[
+            models.Prefetch(
+                query=models.Document(
+                    text=query,
+                    model="jinaai/jina-embeddings-v2-small-en",
+                ),
+                using="jina-small",
+                # Prefetch ten times more results, then
+                # expected to return, so we can really rerank
+                limit=(10 * limit),
+            ),
+        ],
+        query=models.Document(
+            text=query,
+            model="Qdrant/bm25", 
+        ),
+        using="bm25",
+        limit=limit,
+        with_payload=True,
+    )
+
+    return results
+
+
+prompt_template1 = """
+You are a knowledgeable and friendly plant specialist.
+Your expertise covers house plants, their care and toxicity.
+Answer the QUESTION based on the CONTEXT from our plants database.
+Use only the facts from the CONTEXT when answering the QUESTION.
+
+QUESTION: {question}
+
+CONTEXT:
+{context}
+""".strip()
+
+entry_template1 = """
+plant name: {name}
+summary: {summary}
+cultivation: {cultivation}
+toxicity: {toxicity}
+""".strip()
+
+def build_prompt(query, search_results, prompt_template, entry_template):
+    context = ""
+    
+    for doc in search_results:
+        context = context + entry_template.format(**doc) + "\n\n"
+
+    prompt = prompt_template.format(question=query, context=context).strip()
+    return prompt
+
+def gpt_oss_answer(content, model="openai/gpt-oss-20b"):
+  """
+    Generate GPT-OSS model response from Groq Api.
+
+    Args:
+        content (str): Prompt for the model.
+        model (str, optional): Model name. Defaults to "openai/gpt-oss-20b".
+
+    Returns:
+        str: Concatenated response text.
+  """
+  client = Groq(api_key=groq_api_key)
+  completion = client.chat.completions.create(
+      model=model,
+      messages=[
+        {
+          "role": "system",
+          "content": content
+        }
+      ],
+      temperature=1,
+      max_completion_tokens=8192,
+      top_p=1,
+      reasoning_effort="medium",
+      stream=True,
+      stop=None
+  )
+  joined_answer = ''
+  
+  for chunk in completion:
+
+      chunk_answer = chunk.choices[0].delta.content or ""
+      joined_answer = joined_answer + chunk_answer
+    
+  return joined_answer
+
+
+def rag_groq(query, prompt_template, entry_template):
+    """
+    Run RAG with Groq GPT-OSS model.
+
+    Args:
+        query (str): User query to answer.
+        prompt_template (str): Template for constructing the full prompt.
+        entry_template (str): Template for formatting retrieved entries.
+
+    Returns:
+        str: Model-generated answer.
+    """
+    search_results = multi_stage_search(query)
+    search_results_list = []
+
+    for i in search_results.points:
+
+        search_results_list.append(i.payload)
+
+    prompt = build_prompt(query, search_results_list, prompt_template, entry_template)
+    answer = gpt_oss_answer(prompt)
+    
+    return answer
